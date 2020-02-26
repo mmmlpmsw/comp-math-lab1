93bcb0892a2ea2e719f6f80de3b4882fc270fed7
Nadya <mmmlpmsw@gmail.com>
2020-02-27 01:18:57 +0300

    Some changes in calculating the determinant

--- src/mmmlpmsw/comp_math/lab1/Gaussian_elimination/Determinant.java
+++ src/mmmlpmsw/comp_math/lab1/Gaussian_elimination/Determinant.java
@@ -13,52 +13,39 @@ public class Determinant {
     }
 
     private double calculateMatrix(double[][] matrix) {
-        double calcResult = 0d;
-        if (matrix.length == 2) {
-            calcResult = matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
-        }
-        else {
+        double result = 0;
+        if (matrix.length == 2)
+            return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
+
             int flag = 1;
             for (int i = 0; i < matrix.length; i ++) {
-                if (i % 2 == 1) flag =- 1;
-                else flag = 1;
-
-                calcResult += flag * matrix[0][i] * calculateMatrix(getMinor(matrix,0, i));
+//                if (i % 2 == 1) flag =- 1;
+//                else flag = 1;
+                result += flag * matrix[0][i] * calculateMatrix(getMatrixWithoutRowAndColumn(matrix,0, i));
+                flag = -flag;
             }
-        }
-        return calcResult;
+        return result;
     }
 
-    private double[][] getMinor(double[][] matrix, int row, int column) {
-        int minorLength = matrix.length - 1;
-        double[][] minor = new double [minorLength][minorLength];
-
-        //эти переменные для того, чтобы "пропускать" ненужные нам строку и столбец
-        int dI = 0;
-        int dJ = 0;
-        for (int i = 0; i <= minorLength; i ++) {
-            dJ = 0;
-            for (int j = 0; j <= minorLength; j ++) {
-                if (i == row) dI = 1;
-                else {
-                    if (j == column) dJ = 1;
-                    else minor[i-dI][j-dJ] = matrix[i][j];
-                }
-            }
+    private double[][] getMatrixWithoutRowAndColumn(double[][] matrix, int row, int column) {
+        double[][] minor = new double [matrix.length - 1][matrix.length - 1];
+        int offsetRow = 0;
+        int offsetCol = 0;
+        for (int i = 0; i < matrix.length; i ++) {
+            offsetCol = 0;
+            for (int j = 0; j < matrix.length; j ++)
+                if (i == row) offsetRow = 1;
+                else if (j == column) offsetCol = 1;
+                    else minor[i - offsetRow][j - offsetCol] = matrix[i][j];
         }
-
         return minor;
-
     }
 
     private double[][] getArray (LinearSystem system) {
         double [][] array = new double[system.getNumberOfUnknowns()][system.getNumberOfUnknowns() + 1];
-        for (int i = 0; i < system.getNumberOfUnknowns(); i ++) {
-            for (int j = 0; j < system.getNumberOfUnknowns() + 1; j ++) {
+        for (int i = 0; i < system.getNumberOfUnknowns(); i ++)
+            for (int j = 0; j < system.getNumberOfUnknowns() + 1; j ++)
                 array[i][j] = system.getEquationCoefficient(i, j);
-            }
-        }
         return array;
     }
-
 }
